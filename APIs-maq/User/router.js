5c1d4377108db1f89648d430ef825252a391ba51
Muhammad Faisal <muhammad.faisal.ms23@gmail.com>
2023-12-20 14:37:10 +0500

    Remove Old Files maq

--- APIs-maq/User/router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const {signup, singin, alluser, userById} = require("./controller")
-
-router.post("/signup", signup)
-router.post("/login", singin)
-router.get("/alluser", alluser)
-
-module.exports = router
